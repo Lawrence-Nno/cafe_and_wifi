ce73bafd67efcfe2e1319e970a1ddcd364a7d0a1
Lawrence Nno <lawrence.nno@gmail.com>
2024-04-03 13:33:38 +0100

    removed unnecessary comments

--- static/style/style.js
+++ static/style/style.js
@@ -1,18 +1,6 @@
 // JavaScript functions to show/hide the pop-up form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-    // Get the value of the button
-    //var button = document.getElementById('myButton');
-    //var title = button.getAttribute('href');
-    //var anchor = document.getElementById('obi');
-   // var title = anchor.getAttribute('name');
-    // Get the form
-    // var form = document.getElementById('popForm');
-    // Set the form's action to the button value
-    //form.action = title
-
-    // Submit the form (optional)
-    // form.submit();
 }
 
 function closeForm() {
